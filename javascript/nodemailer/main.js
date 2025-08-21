873e9a4da2258283879d3c892dddb17638a4bcb6
Nikolay Matrosov <nikolay.matrosov@gmail.com>
2025-08-21 18:42:14 +0200

    feat: add nodemailer via ses sdk example

--- javascript/nodemailer/main.js
+++ javascript/nodemailer/main.js
@@ -1,6 +1,7 @@
 import nodemailer from "nodemailer";
+import {SendEmailCommand, SESv2Client} from "@aws-sdk/client-sesv2";
 
-async function main() {
+async function smtp() {
     // Create a transporter object with SMTP config
     const transporter = nodemailer.createTransport({
         host: 'postbox.cloud.yandex.net',
@@ -24,4 +25,41 @@ async function main() {
     console.log("Message sent: %s", info.messageId);
 }
 
-main().catch(console.error);
+async function ses() {
+    // 1. Configure the AWS SDK client (uses default credential chain if omitted)
+    const sesClient = new SESv2Client({
+        region: 'ru-central1',
+        endpoint: 'https://postbox.cloud.yandex.net',
+        // By default, the SDK uses the default credentials provider chain.
+        // You can use static credentials by uncommenting and modifying the following lines:
+        // credentials: {
+        //     accessKeyId: 'accessKeyID',
+        //     secretAccessKey: 'secretAccessKey',
+        // },
+    });
+
+    // 2. Create a Nodemailer transport that points at SES
+    const transporter = nodemailer.createTransport({
+        SES: {sesClient, SendEmailCommand},
+    });
+
+    // 3. Send the message
+    const info = await transporter.sendMail({
+        from: '"Your Name" <noreply@ycld.ru>',
+        to: "nikolaymatrosov@gmail.com",
+        subject: "Test Email from Node.js Nodemailer",
+        text: "Hello, this is a plain text message",
+        html: "<b>Hello, this is an HTML message!</b>",
+        // Any SendEmailCommand input can be set under the `ses` key:
+        ses: {
+            // ConfigurationSetName is optional, if you have a configuration set
+            // defined in your AWS SES account, you can specify it here.
+            // ConfigurationSetName: "my‑config‑set",
+            EmailTags: [{Name: "tag_name", Value: "tag_value"}],
+        },
+    });
+}
+
+
+smtp().catch(console.error);
+ses().catch(console.error);
